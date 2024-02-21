ad238bb00316a1aaefac62dd552266e67af4c870
Igor Grafov <grafov2711@gmail.com>
2024-02-21 16:41:49 +0300

    finish project, without the bill of game1

--- script.js
+++ script.js
@@ -1,106 +1,45 @@
-// pseudo code
-/*1. нужно получить кнопки выбора игрока в переменные
-  2. повесить на них слушатель событий, который будет вызывать функцию game(?)
-  3. добавить див для отображения результатов. 
-  4. заменить все console.log на методы Dom
-  5. сделать таблицу счета (игра из пяти раундов)
-*/
-// let
-let userRock = document.querySelector(".rock");
-let userPaper = document.querySelector(".paper");
-let userScissor = document.querySelector(".scissor");
-let userChoice;
-// ui variables
-const PLAYER_BILL = document.querySelector(".player-bill");
-const COMPUTER_BILL = document.querySelector(".computer-bill");
-//const
-// const BTN = document.querySelector(".btn");
-const ROCK = "ROCK";
-const PAPER = "PAPER";
-const SCISSORS = "SCISSORS";
-const DRAW = "Draw";
-const PLAYER_WON = "You are win!";
-const COMPUTER_WON = "Computer has won!";
-
-// addEventListener
-userRock.addEventListener("click", function () {
-  userChoice = ROCK;
-  return userChoice;
-});
-userPaper.addEventListener("click", function () {
-  userChoice = PAPER;
-  return userChoice;
-});
-userScissor.addEventListener("click", function () {
-  userChoice = SCISSORS;
-  return userChoice;
-});
-//// functions
-// get user choice
-// function getUserChoice(value) {
-//   if (value === 0) {
-//     userChoice = ROCK;
-//     return userChoice;
-//   } else if (value === 1) {
-//     userChoice = PAPER;
-//     return userChoice;
-//   } else if (value === 2) {
-//     userChoice = SCISSORS;
-//     return userChoice;
-//   }
-// }
-//
-// function getInput() {
-//   let result;
-//   let userInput = userChoice;
-//   if (userInput) {
-//     userInput = userInput.toUpperCase();
-//     if (userInput === ROCK || userInput === PAPER || userInput === SCISSORS) {
-//       result = userInput;
-//     } else {
-//       alert("We've made a choice for you! It's Rock!");
-//       result = ROCK;
-//     }
-//   } else {
-//     alert("We've made a choice for you! It's Scissors!");
-//     result = SCISSORS;
-//   }
-//   console.log("-----------------");
-//   console.log(`You've selected ${result}!`);
-//   return result;
-// }
 // get computer choice
-function computerInput() {
-  let result = null;
-  const computerChoice = Math.random();
-  if (computerChoice < 0.34) {
-    result = ROCK;
-  } else if (computerChoice < 0.68) {
-    result = SCISSORS;
-  } else result = PAPER;
-  // console.log("-----------------");
-  // console.log(`Computer selected ${result}!`);
-  return result;
+function computerPlay() {
+  const options = ["rock", "scissors", "paper"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return options[randomIndex];
 }
-
-function winner(playerChoice, compChoice) {
-  if (playerChoice == compChoice) {
-    return DRAW;
+// get user input
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw!";
   } else if (
-    (compChoice === ROCK && playerChoice === PAPER) ||
-    (compChoice === PAPER && playerChoice === SCISSORS) ||
-    (compChoice === SCISSORS && playerChoice === ROCK)
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    return PLAYER_WON;
+    return "You are win";
   } else {
-    return COMPUTER_WON;
+    return "Computer has won";
   }
 }
-
-// function game
-function game() {
-  const playerChoice = userChoice;
-  const compChoice = computerInput();
-  console.log(winner(playerChoice, compChoice));
+// функция обновления интерфейса
+function updateGame(playerSelection, computerSelection, result) {
+  const gameDiv = document.querySelector("#game");
+  const resultDiv = document.querySelector("#result");
+  gameDiv.style.display = "none";
+  resultDiv.innerHTML = `You're choice: ${playerSelection}, 
+  computer has choice: ${computerSelection}. ${result}`;
+  const playAgainButton = document.createElement("button");
+  playAgainButton.textContent = "Play Again";
+  playAgainButton.addEventListener("click", () => {
+    gameDiv.style.display = "block";
+    resultDiv.innerHTML = "";
+  });
+  resultDiv.appendChild(playAgainButton);
 }
-game();
+// привязываем функции к кнопкам
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const playerSelection = button.id;
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    updateGame(playerSelection, computerSelection, result);
+  });
+});
